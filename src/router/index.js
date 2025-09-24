89f797068624ffaed03aa60a2b32956ce1986387
jihad <gehadsaed199@gmail.com>
2025-09-24 17:20:37 +0300

    fix: reorder routes in router configuration for improved readability

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/LandingPage.vue'),
+    path: "/",
+    name: "Home",
+    component: () => import("../views/LandingPage.vue"),
   },
-    {
-  path: '/service-details',
-  name: 'ServiceDetails',
-  component: () => import('../views/ServiceDetails.vue'),
-},
   {
-    path: '/business-instructor',
-    name: 'BusinessInstructor',
-    component: () => import('../views/BusinessInstructor.vue')
+    path: "/service-details",
+    name: "ServiceDetails",
+    component: () => import("../views/ServiceDetails.vue"),
   },
- 
-
-]
+  {
+    path: "/business-instructor",
+    name: "BusinessInstructor",
+    component: () => import("../views/BusinessInstructor.vue"),
+    props: (route) => ({
+      token: route.query.token,
+      acs: route.query.acs,
+      user: route.query.user ? JSON.parse(route.query.user) : null,
+    }),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
